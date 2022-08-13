ebcf3368657a06e5523dbf320bebd3b101565344
mukesh <mukesh.s@trois.in>
2022-08-13 17:40:02 +0530

    1st commit

---
+++ TestJenkins/src/test/java/com/testjenkins/TestJenkins/Jenkins.java
@@ -0,0 +1,14 @@
+package com.testjenkins.TestJenkins;
+
+public class Jenkins {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("hello");
+	}
+
+}
