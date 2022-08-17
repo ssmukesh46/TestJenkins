b4dbca9045a444efeb64a3584262b111b62c280b
mukesh <mukesh.s@trois.in>
2022-08-17 17:04:37 +0530

    new test

--- TestJenkins/src/test/java/com/testjenkins/TestJenkins/Jenkins.java
+++ TestJenkins/src/test/java/com/testjenkins/TestJenkins/Jenkins.java
@@ -14,10 +14,9 @@ public class Jenkins {
 		int x = 1;
 		int y = 2;
 		int sum = x + y ;
-		System.out.println(""+sum);
-		System.out.println(""+sum);
+		System.out.println(sum);
 		
-		
+
 		
 		
 		
